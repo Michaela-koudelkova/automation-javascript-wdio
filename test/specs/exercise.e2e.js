c71fd7a8451af7476c5b8121e2f2395fa5310973
Michaela-koudelkova <michaela.koudelkova2017@gmail.com>
2021-11-05 20:16:49 +0100

    Changes

--- test/specs/exercise.e2e.js
+++ test/specs/exercise.e2e.js
@@ -7,9 +7,39 @@ describe('Czechitas Login Page', () => {
         browser.reloadSession();
         
         browser.url('/prihlaseni');
-
-        browser.pause(5000);
         
-    });
+
+        var emailField = $('#email');
+        console.log('Email Field is Displayed  ' + emailField.isDisplayed());
+        console.log('Email Field is Enabled  ' + emailField.isEnabled());
+
+        var passwordField = $('#password');
+        console.log('Password Field is Displayed ' + passwordField.isDisplayed());
+        console.log('Password Field is Enabled ' + passwordField.isEnabled());
+
+        var loginButton = $('button[type="submit"]');
+        console.log("Text přihlašovacího tlačítka " + loginButton.getText());
+
+        var forgotPassword = $('.btn.btn-link');
+        console.log('Hodnota href atributu  ' + forgotPassword.getAttribute("href"));
+
+        emailField.setValue("testerTesterovic@tester.cz");
+        passwordField.setValue("Testing2021");
+        loginButton.click();
+
+        $('=Přihlášky').click();
+
+        var rows = $('table').$("tbody").$$('tr');
+        console.log("Počet řádků tabulky je: " + rows.length);
+
+        for (let i = 0; i < rows.length; i++) {
+            const row = rows[i];
+            console.log(row.getText());
+        }
+
+
+        //browser.pause (5000);
+
+            });
     
 });
--- test/specs/fixtures.js
+++ test/specs/fixtures.js
@@ -1,2 +1,2 @@
-export const username = 'EMAIL';
-export const password = 'PASSWORD!'
+export const username = 'testerTesterovic@tester.cz';
+export const password = 'Testing2021!'
